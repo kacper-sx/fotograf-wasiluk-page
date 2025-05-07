c0255e6dc6cd578bf302ed2c809cc2df815e3647
kacper-sx <kacper.saweczko@o2.pl>
2025-05-07 16:34:13 +0200

    first commit

---
+++ src/components/AppHeader/navData.ts
@@ -0,0 +1,9 @@
+
+
+export const navData = [
+  {href: "/#services", title: "O mnie"},
+  {href: "/#about", title: "Fotografia ślubna"},
+  {href: "/#collaboration", title: "Film ślubny"},
+  {href: "/#galley", title: "Galeria"},
+  {href: "/#contact", title: "Kontakt"},
+]
