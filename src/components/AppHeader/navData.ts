a1c19b5013f6f044fbcb2b887d09f0c460cd7941
kacper-sx <kacper.saweczko@o2.pl>
2025-05-12 16:43:13 +0200

    feat: add gallery

--- src/components/AppHeader/navData.ts
+++ src/components/AppHeader/navData.ts
@@ -4,6 +4,6 @@ export const navData = [
   {href: "/#about", title: "O mnie"},
   {href: "/#photos", title: "Fotografia ślubna"},
   {href: "/#video", title: "Film ślubny"},
-  {href: "/#galley", title: "Galeria"},
+  {href: "/galeria", title: "Galeria"},
   {href: "/#contact", title: "Kontakt"},
 ]
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,13 +1,17 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
 
 export const qsAll = <T extends string>(selector: T) => {
   const els = document.querySelectorAll(selector)
   return Array.from(els).filter(el => el instanceof HTMLElement)
 } 
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
+export const dqsAll = (selector: string) =>
+  Array.from(document.querySelectorAll(selector)).filter(isHTMLElement);
+
+export const isHTMLElement = (el: unknown): el is HTMLElement =>
+  el instanceof HTMLElement;
